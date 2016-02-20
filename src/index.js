697de98177c56d1819d4e861aa7b0116dc29d946
Hutson Betts <hutson@hyper-expanse.net>
2016-02-19 22:39:35 -0600

    feat(index): Upgrade to conventional-github-releaser.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 'use strict';
 
-var _ = require('lodash');
-var bluebird = require('bluebird');
-var url = require('url');
-var GitHubApi = require('github');
-var through = require('through2');
-var parseGithubUrl = require('@bahmutov/parse-github-repo-url');
-var commitParser = require('./commit-parser');
+var conventionalGithubReleaser = require('conventional-github-releaser');
 
-module.exports = githubNotifier;
+module.exports = githubReleaser;
 
-function githubNotifier(pluginConfig, config, callback) {
-  var githubConfig = config.options.githubUrl ? url.parse(config.options.githubUrl) : {};
-
-  var github = new GitHubApi({
-    version: '3.0.0',
-    port: githubConfig.port,
-    protocol: (githubConfig.protocol || '').split(':')[0] || null,
-    host: githubConfig.hostname,
-  });
-
-  github.authenticate({
+function githubReleaser(pluginConfig, config, callback) {
+  var auth = {
     type: 'oauth',
     token: config.options.githubToken,
-  });
+  };
 
   // Placed at the end so that all GitHub code has had a chance to be invoked, including sanity
   // checking for required input, like a GitHub token.
@@ -31,33 +16,14 @@ function githubNotifier(pluginConfig, config, callback) {
     return callback(null);
   }
 
-  var createComment = bluebird.promisify(github.issues.createComment);
-  var parsedGithubUrl = parseGithubUrl(config.pkg.repository.url);
-  commitParser(config.commits)
-    .pipe(through.obj(function (commit, enc, cb) {
-      var commentPromises = _.map(commit.references, function (reference) {
-        var msg = {
-          user: parsedGithubUrl[0],
-          repo: parsedGithubUrl[1],
-          number: reference.issue,
-          message: 'Version ' + config.pkg.version + ' has been published.',
-        };
-
-        return createComment(msg);
-      });
-
-      bluebird.all(commentPromises)
-        .then(function () {
-          cb(null, commit);
-        })
-        .catch(function (err) {
-          cb(err);
-        });
-    }))
-    .on('error', function () {
+  conventionalGithubReleaser(auth, {
+    preset: config.preset || 'angular',
+  }, function (err/*, responses*/) {
+    console.log('err', err);
+    if (err) {
       callback(false);
-    })
-    .on('finish', function () {
-      callback(true);
-    });
+    }
+
+    callback(true);
+  });
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -9,22 +9,17 @@ var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 var expect = chai.expect;
 
-function githubMock() {
-}
-
-githubMock.prototype.authenticate = sinon.spy();
-githubMock.prototype.issues = {
-  createComment: sinon.stub(),
-};
-
 describe('semantic-release-github-notifier', function () {
   var options;
   var plugin;
+  var conventionalGithubReleaserMock = sinon.stub();
 
   before(function () {
     nock.disableNetConnect();
 
-    plugin = proxyquire('./index', { github: githubMock });
+    plugin = proxyquire('./index', {
+      'conventional-github-releaser': conventionalGithubReleaserMock,
+    });
   });
 
   beforeEach(function () {
@@ -34,89 +29,31 @@ describe('semantic-release-github-notifier', function () {
       githubUrl: 'https://www.github.com:80',
     };
 
-    githubMock.prototype.authenticate.reset();
-    githubMock.prototype.issues.createComment.reset();
+    conventionalGithubReleaserMock.reset();
   });
 
-  describe('debug mode', function () {
+  it('does not release in debug mode', function () {
+    var callback = sinon.spy();
 
-    it('parses GitHub URL', function () {
-      var callback = sinon.spy();
+    plugin({}, { options: options }, callback);
 
-      plugin({}, { options: options }, callback);
+    expect(conventionalGithubReleaserMock).to.not.have.been.called;
 
-      expect(githubMock.prototype.authenticate).to.have.been.calledOnce
-        .and.to.have.been.calledWithExactly({ type: 'oauth', token: options.githubToken });
-
-      expect(callback).to.have.been.calledOnce
-        .and.to.have.been.calledWithExactly(null);
-    });
-
-    it('falls back on default GitHub URL', function () {
-      var callback = sinon.spy();
-      options.githubUrl = undefined;
-
-      plugin({}, { options: options }, callback);
-
-      expect(githubMock.prototype.authenticate).to.have.been.calledOnce
-        .and.to.have.been.calledWithExactly({ type: 'oauth', token: options.githubToken });
-
-      expect(callback).to.have.been.calledOnce
-        .and.to.have.been.calledWithExactly(null);
-    });
+    expect(callback).to.have.been.calledOnce
+      .and.to.have.been.calledWithExactly(null);
   });
 
   describe('normal mode', function () {
 
-    function pluginWrapper(callback) {
-      plugin({}, {
-        commits: [
-          {
-            message: 'feat(something): Added new feature.\nFixes #1.',
-          },
-        ],
-        options: options,
-        pkg: {
-          repository: {
-            url: 'https://github.com/hbetts/semantic-release-github-notifier.git',
-          },
-          version: '1.0.0',
-        },
-      }, callback);
-    }
-
-    beforeEach(function () {
+    it('conventional-github-releaser called with default preset', function (done) {
       options.debug = undefined;
-    });
+      conventionalGithubReleaserMock.yields(null, []);
 
-    it('calls callback with true on successful GitHub comment', function (done) {
-      githubMock.prototype.issues.createComment.onFirstCall().callsArgWith(1, null, {});
-
-      pluginWrapper(pluginCallback);
+      plugin({}, { options: options }, pluginCallback);
 
       function pluginCallback(result) {
-        expect(githubMock.prototype.issues.createComment).to.have.been.calledOnce
-          .and.to.have.been.calledWith({
-            user: 'hbetts',
-            repo: 'semantic-release-github-notifier',
-            number: '1',
-            message: 'Version 1.0.0 has been published.',
-          });
+        expect(conventionalGithubReleaserMock).to.have.been.calledOnce;
         expect(result).to.equal(true);
-
-        done();
-      }
-    });
-
-    it('calls callback with false on failed GitHub comment', function (done) {
-      githubMock.prototype.issues.createComment.onFirstCall().callsArgWith(1, new Error('Failed'), {});
-
-      pluginWrapper(pluginCallback);
-
-      function pluginCallback(result) {
-        expect(githubMock.prototype.issues.createComment).to.have.been.calledOnce;
-        expect(result).to.equal(false);
-
         done();
       }
     });
